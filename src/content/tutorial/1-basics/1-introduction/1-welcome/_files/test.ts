e7394923e34469378837ef896ece53f75a0662db
Noam Honig <noam.honig@gmail.com>
2024-08-05 08:12:22 +0300

    typescript typing

--- src/content/tutorial/1-basics/1-introduction/1-welcome/_files/test.ts
+++ src/content/tutorial/1-basics/1-introduction/1-welcome/_files/test.ts
@@ -1,4 +1,12 @@
-import { otherFunction } from "./other-function";
+type Person = {
+  firstName: string;
+  lastName: string;
+};
+
 export function test() {
-  otherFunction();
+  var p: Person = {
+    // try typing firstName / lastName - I don't get any typing errors
+  };
+
+  alert(p.firstName + " " + p.lastName);
 }
